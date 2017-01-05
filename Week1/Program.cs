bb96a6bd5f221c630c4b70f4d3b4b67a71f74ce4
brianrichardmccarthy <brianmccarthy1@outlook.ie>
2017-01-05 00:10:59 +0000

    Added Week 1 assignment

---
+++ Week1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1 {
+
+    /// <summary>
+    /// Program to do something
+    /// Author: Brian McCarthy
+    /// </summary>
+    class Program {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args">Command Line Arguments</param>
+        static void Main(string[] args) {
+        }
+    }
+}
