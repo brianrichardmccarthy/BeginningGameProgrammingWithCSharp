c3b7f4320bd274d57459c6fbf7091a8f196f3ca3
brianrichardmccarthy <brianmccarthy1@outlook.ie>
2017-01-05 00:12:19 +0000

    Added programming asignment 2

---
+++ ProgrammingAssignment2/ProgrammingAssignment2/Game1.cs
@@ -0,0 +1,176 @@
+using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ProgrammingAssignment2
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        const int WindowWidth = 800;
+        const int WindowHeight = 600;
+
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        // STUDENTS: declare variables for three sprites
+        Texture2D sprite0;
+        Texture2D sprite1;
+        Texture2D sprite2;
+
+        // STUDENTS: declare variables for x and y speeds
+        int speedX, speedY;
+
+        // used to handle generating random values
+        Random rand = new Random();
+        const int ChangeDelayTime = 1000;
+        int elapsedTime = 0;
+
+        // used to keep track of current sprite and location
+        Texture2D currentSprite;
+        Rectangle drawRectangle = new Rectangle();
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = WindowWidth;
+            graphics.PreferredBackBufferHeight = WindowHeight;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // STUDENTS: load the sprite images here
+            sprite0 = Content.Load<Texture2D>((@"graphics\teddybear0"));
+            sprite1 = Content.Load<Texture2D>((@"graphics\teddybear1"));
+            sprite2 = Content.Load<Texture2D>((@"graphics\teddybear2"));
+
+            // STUDENTS: set the currentSprite variable to one of your sprite variables
+            currentSprite = sprite0;
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            elapsedTime += gameTime.ElapsedGameTime.Milliseconds;
+            if (elapsedTime > ChangeDelayTime)
+            {
+                elapsedTime = 0;
+
+                // STUDENTS: uncomment the code below and make it generate a random number 
+                // between 0 and 2 inclusive using the rand field I provided
+
+                // get a new int betweenn 0 and 2
+                int spriteNumber = rand.Next(0, 3);
+
+                // sets current sprite
+                // STUDENTS: uncomment the lines below and change sprite0, sprite1, and sprite2
+                //      to the three different names of your sprite variables
+                if ( spriteNumber == 0 ) {
+                    currentSprite = sprite0;
+                } else if ( spriteNumber == 1 ) {
+                    currentSprite = sprite1;
+                } else if ( spriteNumber == 2 ) {
+                    currentSprite = sprite2;
+                }
+
+                // STUDENTS: set the drawRectangle.Width and drawRectangle.Height to match the width and height of currentSprite
+
+                // change the length and width of the rentangle to the current sprite
+                drawRectangle.Width = currentSprite.Width;
+                drawRectangle.Height = currentSprite.Height;
+
+                // STUDENTS: center the draw rectangle in the window. Note that the X and Y properties of the rectangle
+                // are for the upper left corner of the rectangle, not the center of the rectangle
+
+                // center the rentangle
+                drawRectangle.X = (WindowWidth / 2) - (drawRectangle.Width / 2);
+                drawRectangle.Y = (WindowHeight / 2) - (drawRectangle.Height / 2);
+
+                // STUDENTS: write code below to generate random numbers  between -4 and 4 inclusive for the x and y speed 
+                // using the rand field I provided
+                // CAUTION: Don't redeclare the x speed and y speed variables here!
+                
+                // random int between -4 and 4
+                speedX = rand.Next(-4, 5);
+                speedY = rand.Next(-4, 5);
+            }
+
+            // STUDENTS: move the drawRectangle by the x speed and the y speed
+            
+            // add the speedX to the drawRectangle X
+            drawRectangle.X += speedX;
+
+            // add the speedY to the drawRectangle Y
+            drawRectangle.Y += speedY;
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // STUDENTS: draw current sprite here
+
+            // start the spriteBatch
+            spriteBatch.Begin();
+
+            // draw the current sprite in the retangle
+            spriteBatch.Draw(currentSprite, drawRectangle, Color.White);
+
+            // end the spriteBatch
+            spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
